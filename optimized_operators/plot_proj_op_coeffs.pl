34ba89f341d845258fcbef045d8fdca4fdccaa53
Christian Shultz <shultz@jlab.org>
2014-07-09 12:11:09 -0400

    stuff for raul

--- optimized_operators/plot_proj_op_coeffs.pl
+++ optimized_operators/plot_proj_op_coeffs.pl
@@ -61,8 +61,8 @@ foreach my $plot (@plots)
   my $p = $a[0]; 
   my $f = $a[1]; 
   system("/home/shultz/git-builds/gnuplot/bin/gnuplot -persist $p"); 
-  unlink $p; 
-  unlink $f; 
+#  unlink $p; 
+#  unlink $f; 
 }
 
 
@@ -78,104 +78,86 @@ sub make_gp
 
   my ($opr,$sgnr) = normalize(\@dat); 
 
-my %ops = %{$opr};
-my %sgns = %{$sgnr};
-my @keys = keys %ops; 
+  my %ops = %{$opr};
+  my %sgns = %{$sgnr};
+  my @keys = keys %ops; 
 
 # a sort based on spins
-my @j0 = ();
-my @j1 = ();
-my @j2 = ();
-my @j3 = ();
-my @j4 = (); 
+  my @j0 = ();
+  my @j1 = ();
+  my @j2 = ();
+  my @j3 = ();
+  my @j4 = (); 
 
 # assuming standard operator names __J#
-foreach my $key (@keys)
-{
-  push @j0, $key if $key =~ m/__J0/;
-  push @j1, $key if $key =~ m/__J1/;
-  push @j2, $key if $key =~ m/__J2/;
-  push @j3, $key if $key =~ m/__J3/;
-  push @j4, $key if $key =~ m/__J4/;
-}
+  foreach my $key (@keys)
+  {
+    push @j0, $key if $key =~ m/__J0/;
+    push @j1, $key if $key =~ m/__J1/;
+    push @j2, $key if $key =~ m/__J2/;
+    push @j3, $key if $key =~ m/__J3/;
+    push @j4, $key if $key =~ m/__J4/;
+  }
 
 # set up a tag for this running of the perl script
-my $file = "tmp/".$tag . "_" .$rn;
+  my $file = "tmp/".$tag . "_" .$rn;
 
 
 # dump the data into a file for gnuplot
-my $fout = $file . "_" . ".dat";
-open DATOUT , ">" , $fout;
+  my $fout = $file . "_" . ".dat";
+  open DATOUT , ">" , $fout;
 
-my $xp = 0.; 
-my $j0f = dump_data($file,0,\@j0,\%ops,\%sgns);
-my $j1f = dump_data($file,1,\@j1,\%ops,\%sgns); 
-my $j2f = dump_data($file,2,\@j2,\%ops,\%sgns); 
-my $j3f = dump_data($file,3,\@j3,\%ops,\%sgns); 
-my $j4f = dump_data($file,4,\@j4,\%ops,\%sgns); 
+  my $xp = 0.; 
+  my $j0f = dump_data($file,0,\@j0,\%ops,\%sgns);
+  my $j1f = dump_data($file,1,\@j1,\%ops,\%sgns); 
+  my $j2f = dump_data($file,2,\@j2,\%ops,\%sgns); 
+  my $j3f = dump_data($file,3,\@j3,\%ops,\%sgns); 
+  my $j4f = dump_data($file,4,\@j4,\%ops,\%sgns); 
 
-close DATOUT;
+  close DATOUT;
 
 
 # make the plot
-my $plot = $file . ".gp";
+  my $plot = $file . ".gp";
 
-open OUT , ">" , $plot; 
+  open OUT , ">" , $plot; 
 
-# kill any user defined styles, gnuplot 4.7 was 
-#      having a hard time with this and the 
-#      labels were not matching the boxplot
-print OUT "unset linetype 0 \n";
-print OUT "unset linetype 1 \n";
-print OUT "unset linetype 2 \n";
-print OUT "unset linetype 3 \n";
-print OUT "unset linetype 4 \n";
-print OUT "set xtics nomirror rotate by -45 font \",8\" \n";
-print OUT "set key outside \n"; 
-print OUT "set style data histogram \n";
-print OUT "set style fill solid border -1 \n";
-print OUT "set title \"$tag\" \n";
-print OUT "unset key \n";
-print OUT "set rmargin at screen 0.85 \n";
-print OUT "set yrange [0:1.05] \n";
-print OUT "set object 1 rect from screen 0.90,0.85 to screen 0.95,0.90 fc lt 0 \n";
-print OUT "set object 2 rect from screen 0.90,0.75 to screen 0.95,0.80 fc lt 1 \n";
-print OUT "set object 3 rect from screen 0.90,0.65 to screen 0.95,0.70 fc lt 2 \n";
-print OUT "set object 4 rect from screen 0.90,0.55 to screen 0.95,0.60 fc lt 3 \n";
-print OUT "set object 5 rect from screen 0.90,0.45 to screen 0.95,0.50 fc lt 4 \n";
-print OUT "set label 1 at screen 0.915,0.87 \"J0\" front \n";
-print OUT "set label 2 at screen 0.915,0.77 \"J1\" front \n";
-print OUT "set label 3 at screen 0.915,0.67 \"J2\" front \n";
-print OUT "set label 4 at screen 0.915,0.57 \"J3\" front \n";
-print OUT "set label 5 at screen 0.915,0.47 \"J4\" front \n";
-print OUT "plot \"$fout\" u (column(0)):2:(0.5):(\$3):xtic(1) w boxes lc variable, \\\n" ;
+  print OUT "set key noenhanced\n";
+  print OUT "set bars fullwidth \n";
+  print OUT "set style data histogram \n";
+  print OUT "set style fill solid border -1 \n";
+  print OUT "set style histogram errorbars gap 5 lw 1 \n";
+  print OUT "set xtics nomirror rotate by -45 font \",8\"\n";
+  print OUT "set title \"$tag\" \n";
+  print OUT "set yrange [0:1.05] \n";
+  print OUT "plot \"$fout\" u (column(0)):2:xtic(1), \\\n" ;
 #                                #xval:ydata:boxwidth:color_index:xtic_labels
-print OUT "\"$fout\" u (column(0)):(\$2 + 0.02):4 with labels";
+  print OUT "\"$fout\" u (column(0)):(\$2 + 0.02):4 with labels";
 
 
-if($saveplot)
-{
+  if($saveplot)
+  {
 # looks like crap
 #  my $png = $file . ".png";
 # print OUT "\nset output \"$png\" \n";
 #  print OUT "set term png\n";
 
-  my $ps = $tag . ".eps";
-  print OUT "\nset terminal postscript eps \n";
-  print OUT "set output \"$ps\" \n";
-  print OUT "replot"
+    my $ps = $tag . ".eps";
+    print OUT "\nset terminal postscript eps \n";
+    print OUT "set output \"$ps\" \n";
+    print OUT "replot"
 
-}
+  }
 
 
 #print OUT "plot \"$fout\" using xticlabels(1):2:3   \n"; 
 
-close OUT; 
+  close OUT; 
 
 
-my @r = ( $plot , $fout ) ; 
+  my @r = ( $plot , $fout ) ; 
 
-return \@r; 
+  return \@r; 
 }
 
 
@@ -246,5 +228,182 @@ sub dump_data
 
   return $#keys; 
 }
+
+
+# all credit to JJD, stolen code 
+sub ident_op {
+  # attempts to color code operators 
+  my $op = $_[0];
+
+  # fixing up jo's code, need to predeclare the 
+  # rat's nest of variables since we are not 
+  # abusing perls malfeatures 
+  my $hadron = "";
+  my $isospin = "";
+  my $flavspin; 
+  my $deriv; 
+  my $spin; 
+  my $type; 
+  my $opname; 
+  my $opstruct; 
+  my $spinirrep; 
+  my $flavdirac; 
+  my @aflavdirac; 
+  my $dirac; 
+  my $flav; 
+  my $flavrep; 
+
+  # add another dimension to these maps for flavour rep (eventually)
+
+  # baryon colors
+  my %baryon_colors = 
+  (
+    'J1o2' => {'conv' => '"black"', 
+      'hyb' => '"#A9A9A9"', 
+      'D2J1A'=> '"#DCDCDC"', 
+      'local'=>'"#708090"', 
+      'lapl'=>'"#F0FFFF"'},
+    'J3o2' => {'conv' => '"red"', 
+      'hyb' => '"#FF1493"', 
+      'D2J1A'=> '"#D2691E"', 
+      'local'=>'"#A52A2A"'},
+    'J5o2' => {'conv' => '"#228B22"', 
+      'hyb' => '"#7FFF00"', 
+      'D2J1A' => 
+      '"#ADFF2F"' },
+    'J7o2' => {'conv' => '"blue"', 
+      'hyb' => '"#1E90FF"', 
+      'D2J1A'=>'"#20B2AA"'}
+  );
+
+  # meson colors
+  my %meson_colors = (
+    'J0' => {'conv' => '"black"', 
+      'hyb' => '"#A9A9A9"'},
+    'J1' => {'conv' => '"red"', 
+      'hyb' => '"#FF1493"'},
+    'J2' => {'conv' => '"#228B22"', 
+      'hyb' => '"#7FFF00"'},
+    'J3' => {'conv' => '"blue"', 
+      'hyb' => '"#1E90FF"'},
+    'J4' => {'conv' => '"gold"'},
+    'other' => {'other' => '"brown"'}    );
+
+  my %colors = (%baryon_colors, %meson_colors);
+
+
+  my @isoscalarnames = ('l', 's', 'octet', 'singlet', 'fl', 'fs', 'f1', 'f8', 'etal', 'etas', 'eta1', 'eta8', 'hl', 'hs', 'h1', 'h8', 'omegal', 'omegas', 'omega1', 'omega8');
+
+  #baryon syntax   NucleonMG1g1MxD0J0S_J1o2_G1g1
+  #meson syntax    b0xD1_J1__J1_T1 (old)   or   rho_rhoxD0_J0__J1_T1
+
+  my @break = split('_', $op);
+  my $irrep = &removeHelicity($break[-1]);
+
+  if( ($irrep =~ /G/) || ($irrep =~ /H/) ||
+    ($irrep =~ /D4E1/) || ($irrep =~ /D4E3/) ||
+    ($irrep =~ /D3E1/) || ($irrep =~ /D3B/) ||
+    ($irrep =~ /D2E/) ||
+    ($irrep =~ /C4E/)) 
+  {
+    #its a baryon
+    $hadron = "baryon";
+    ($opname, $spin, $irrep) = split('_', $op);
+    ($flavspin, $deriv) = split('x', $opname);
+  }
+  elsif( ($irrep =~ /A/) || ($irrep =~ /T/) || ($irrep =~ /E/) ||
+    ($irrep =~ /D4A/) || ($irrep =~ /D4E2/) || ($irrep =~ /D4B/) ||
+    ($irrep =~ /D3A/) || ($irrep =~ /D3E2/) ||
+    ($irrep =~ /D2A/) || ($irrep =~ /D2B/) ||
+    ($irrep =~ /C4A/)) 
+  {
+    #its a meson
+    $hadron = "meson";
+    ($opstruct, $spinirrep) = split('__', $op);
+    ($spin, $irrep) = split('_', $spinirrep);
+    ($flavdirac, $deriv) = split('x', $opstruct);
+
+    @aflavdirac =  split('_', $flavdirac);
+
+    $isospin = 1;
+
+    if( scalar(@aflavdirac) == 1 )
+    {
+      # old op names
+      $dirac = $aflavdirac[0];
+    }
+    else
+    {
+      $flav = $aflavdirac[0];
+      $dirac = $aflavdirac[1];
+
+      if (grep {$_ eq $flav} @isoscalarnames) {
+        $isospin = 0;
+      }
+    }
+
+  }
+  else
+  {
+    # will probably be called for two-particle opes
+    print "don't know how to deal with $op yet! coloring it brown \n";
+    my $spin = "other";
+  }
+
+  # decided if baryon,meson,or other
+  if($hadron eq 'baryon')
+  {
+    # flavor reps not used yet
+    if($flavspin =~ m/Sigma8/ )
+    {
+      $flavrep = 8;
+    }
+    elsif($flavspin =~ m/Sigma10/ )
+    {
+      $flavrep = 10;
+    }
+    #need extra coloration to show flavor
+    #grow the map
+
+    #baryon deriv types
+    $type = "conv";
+    if( ($deriv eq 'D2J1S') || ($deriv eq 'D2J1M') ){ $type = "hyb"; }
+    elsif( $deriv eq 'D2J1A'){ $type = "D2J1A"; }
+    elsif( $deriv eq 'D0J0S'){ $type = "local"; }
+#    elsif ($deriv eq 'D2J0S'){ $type = "lapl"; }
+  }
+  elsif($hadron eq 'meson')
+  {
+    # flavour reps not used yet
+    if($isospin == 1)
+    {
+      #don't need coloring by flavor
+    }
+    else
+    {
+      #need extra coloration to show flavor
+      #grow the map
+    }
+
+    # meson deriv types
+    $type = "conv";
+    if( ( $deriv eq 'D2_J1') || ($deriv eq 'D3_J132_J2') ||  ($deriv eq 'D3_J131_J0') 
+      ||  ($deriv eq 'D3_J131_J1') ||  ($deriv eq 'D3_J131_J2') ) 
+    { 
+      $type = "hyb"; 
+    }   
+
+  }
+  else
+  {
+    print "DON'T KNOW WHAT TO DO WITH THIS OPERATOR : ${op} -> brown \n";
+    $type = "other";
+  }
+
+
+  my $color = $colors{$spin}{$type};
+  return $color;
+}
+
 
 
---
+++ optimized_operators/szscl21_20_128_b1p50_t_x4p300_um0p0840_sm0p0743_n1p265/pion/proj0/proj_op_pion_proj0.pl
@@ -0,0 +1,123 @@
+#!/usr/bin/perl -w
+#
+# this is some auto generated perl skeleton
+# and should not be assumed to be correct or
+# work in the way you think it should
+#
+
+
+use strict; 
+use OPparams; 
+use File::Basename;
+
+my $basedir = dirname($0); 
+
+require "${basedir}/OPparams.work.2particle.pl";
+
+my @all_ops = (); 
+
+
+  my $pion_proj0_p000_A1 = OPparams->new();
+  $pion_proj0_p000_A1->pid("pion_proj0");
+  $pion_proj0_p000_A1->irrep("A1");
+  $pion_proj0_p000_A1->irrep_stem("A1");
+  $pion_proj0_p000_A1->mom("p000"); 
+  $pion_proj0_p000_A1->twoI_z(2); 
+  $pion_proj0_p000_A1->ncfg(603);
+  $pion_proj0_p000_A1->ensemble("szscl21_20_128_b1p50_t_x4p300_um0p0840_sm0p0743_n1p265"); 
+  $pion_proj0_p000_A1->recon_dir("/work/JLabLQCD/LHPC/Spectrum/Clover/NF2+1/szscl21_20_128_b1p50_t_x4p300_um0p0840_sm0p0743_n1p265/meson_2pt/");
+  $pion_proj0_p000_A1->t0(-1);
+  $pion_proj0_p000_A1->state(-1); 
+  $pion_proj0_p000_A1->tz(-1);
+  $pion_proj0_p000_A1->phaser(1.);
+  $pion_proj0_p000_A1->spin(0);
+  $pion_proj0_p000_A1->nested(undef); 
+
+  my $pion_proj0_p100_H0D4A2 = OPparams->new();
+  $pion_proj0_p100_H0D4A2->pid("pion_proj0");
+  $pion_proj0_p100_H0D4A2->irrep("D4A2");
+  $pion_proj0_p100_H0D4A2->irrep_stem("H0D4A2");
+  $pion_proj0_p100_H0D4A2->mom("p100"); 
+  $pion_proj0_p100_H0D4A2->twoI_z(2); 
+  $pion_proj0_p100_H0D4A2->ncfg(603);
+  $pion_proj0_p100_H0D4A2->ensemble("szscl21_20_128_b1p50_t_x4p300_um0p0840_sm0p0743_n1p265"); 
+  $pion_proj0_p100_H0D4A2->recon_dir("/work/JLabLQCD/LHPC/Spectrum/Clover/NF2+1/szscl21_20_128_b1p50_t_x4p300_um0p0840_sm0p0743_n1p265/meson_2pt/");
+  $pion_proj0_p100_H0D4A2->t0(-1);
+  $pion_proj0_p100_H0D4A2->state(-1); 
+  $pion_proj0_p100_H0D4A2->tz(-1);
+  $pion_proj0_p100_H0D4A2->phaser(1.);
+  $pion_proj0_p100_H0D4A2->spin(0);
+  $pion_proj0_p100_H0D4A2->nested(undef); 
+
+  my $pion_proj0_p110_H0D2A2 = OPparams->new();
+  $pion_proj0_p110_H0D2A2->pid("pion_proj0");
+  $pion_proj0_p110_H0D2A2->irrep("D2A2");
+  $pion_proj0_p110_H0D2A2->irrep_stem("H0D2A2");
+  $pion_proj0_p110_H0D2A2->mom("p110"); 
+  $pion_proj0_p110_H0D2A2->twoI_z(2); 
+  $pion_proj0_p110_H0D2A2->ncfg(603);
+  $pion_proj0_p110_H0D2A2->ensemble("szscl21_20_128_b1p50_t_x4p300_um0p0840_sm0p0743_n1p265"); 
+  $pion_proj0_p110_H0D2A2->recon_dir("/work/JLabLQCD/LHPC/Spectrum/Clover/NF2+1/szscl21_20_128_b1p50_t_x4p300_um0p0840_sm0p0743_n1p265/meson_2pt/");
+  $pion_proj0_p110_H0D2A2->t0(-1);
+  $pion_proj0_p110_H0D2A2->state(-1); 
+  $pion_proj0_p110_H0D2A2->tz(-1);
+  $pion_proj0_p110_H0D2A2->phaser(1.);
+  $pion_proj0_p110_H0D2A2->spin(0);
+  $pion_proj0_p110_H0D2A2->nested(undef); 
+
+  my $pion_proj0_p111_H0D3A2 = OPparams->new();
+  $pion_proj0_p111_H0D3A2->pid("pion_proj0");
+  $pion_proj0_p111_H0D3A2->irrep("D3A2");
+  $pion_proj0_p111_H0D3A2->irrep_stem("H0D3A2");
+  $pion_proj0_p111_H0D3A2->mom("p111"); 
+  $pion_proj0_p111_H0D3A2->twoI_z(2); 
+  $pion_proj0_p111_H0D3A2->ncfg(603);
+  $pion_proj0_p111_H0D3A2->ensemble("szscl21_20_128_b1p50_t_x4p300_um0p0840_sm0p0743_n1p265"); 
+  $pion_proj0_p111_H0D3A2->recon_dir("/work/JLabLQCD/LHPC/Spectrum/Clover/NF2+1/szscl21_20_128_b1p50_t_x4p300_um0p0840_sm0p0743_n1p265/meson_2pt/");
+  $pion_proj0_p111_H0D3A2->t0(-1);
+  $pion_proj0_p111_H0D3A2->state(-1); 
+  $pion_proj0_p111_H0D3A2->tz(-1);
+  $pion_proj0_p111_H0D3A2->phaser(1.);
+  $pion_proj0_p111_H0D3A2->spin(0);
+  $pion_proj0_p111_H0D3A2->nested(undef); 
+
+  my $pion_proj0_p200_H0D4A2 = OPparams->new();
+  $pion_proj0_p200_H0D4A2->pid("pion_proj0");
+  $pion_proj0_p200_H0D4A2->irrep("D4A2");
+  $pion_proj0_p200_H0D4A2->irrep_stem("H0D4A2");
+  $pion_proj0_p200_H0D4A2->mom("p200"); 
+  $pion_proj0_p200_H0D4A2->twoI_z(2); 
+  $pion_proj0_p200_H0D4A2->ncfg(603);
+  $pion_proj0_p200_H0D4A2->ensemble("szscl21_20_128_b1p50_t_x4p300_um0p0840_sm0p0743_n1p265"); 
+  $pion_proj0_p200_H0D4A2->recon_dir("/work/JLabLQCD/LHPC/Spectrum/Clover/NF2+1/szscl21_20_128_b1p50_t_x4p300_um0p0840_sm0p0743_n1p265/meson_2pt/");
+  $pion_proj0_p200_H0D4A2->t0(-1);
+  $pion_proj0_p200_H0D4A2->state(-1); 
+  $pion_proj0_p200_H0D4A2->tz(-1);
+  $pion_proj0_p200_H0D4A2->phaser(1.);
+  $pion_proj0_p200_H0D4A2->spin(0);
+  $pion_proj0_p200_H0D4A2->nested(undef); 
+
+
+  my @all_operators = (); 
+  push @all_operators , $pion_proj0_p000_A1; 
+  push @all_operators , $pion_proj0_p100_H0D4A2; 
+  push @all_operators , $pion_proj0_p110_H0D2A2; 
+  push @all_operators , $pion_proj0_p111_H0D3A2; 
+  push @all_operators , $pion_proj0_p200_H0D4A2; 
+
+
+  my @extracts = (); 
+  my @list_extracts = (); 
+  foreach my $op (@all_operators)
+  {
+    push @extracts,  &run_extract_all_v_coeffs_xml($op); 
+    push @list_extracts , &run_extract_all_v_coeffs_svd($op); 
+  }
+
+  my $listfile = &finish_proj_xml(\@extracts,\@list_extracts,"pion_proj0"); 
+
+  &make_proj_plots($listfile);
+
+  &write_radmat_xml(\@all_operators); 
+  
+  &serialize_ops_list( "pion_proj0.perl.xml" , \@all_operators ); 
